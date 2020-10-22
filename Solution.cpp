73857d32dd49984f28a6cccc13e9fb83e26b523a
ayush-swm <ayushagrawal.swm@gmail.com>
2020-10-22 22:53:13 +0530

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -6,12 +6,12 @@
 #include <regex>
 using namespace std;
 
-const int IGNORE_CASE = 1<<0;
-const int INVERT_MATCH = 1<<1;
-const int MATCH_WHOLE_WORD = 1<<2;
-const int PRINT_LINE_NO = 1<<3;
+const int IGNORE_CASE = 1 << 0;
+const int INVERT_MATCH = 1 << 1;
+const int MATCH_WHOLE_WORD = 1 << 2;
+const int PRINT_LINE_NO = 1 << 3;
 
-void prinHelp(){
+void prinHelp() {
 	cout<<"valid command"<<endl;
 	cout<<"  [options] [patern] [file name]"<<endl;
 	cout<<"  eg. line input.txt"<<endl;
@@ -25,52 +25,55 @@ void prinHelp(){
 	return;
 }
 
-bool isalphaNum(char c){
-	if((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9') || c=='_')
-		return true;
+bool isalphaNum(char c) {
+	if((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9') || c=='_') {
+	    return true;
+	}
 	return false;
 }
 
-void printMatch(int& flag, string patternToSearch, string fileName){
+void printMatch(int& flag, string patternToSearch, string fileName) {
 	ifstream file;
     file.open(fileName.c_str());
-    if(!file) {
+    if (!file) {
        cout<<"File " + fileName + " not present at current directory."<<endl;
        return;
     }
-    regex expr = flag&IGNORE_CASE ? regex(patternToSearch, regex_constants::icase) : regex(patternToSearch); 
+    regex expr = flag & IGNORE_CASE ? regex(patternToSearch, regex_constants::icase)
+	        : regex(patternToSearch); 
 	string lineData="";
 	int lineNo=0;
-	while(getline(file, lineData)){
+	while (getline(file, lineData)) {
 		lineNo++;
 		bool match = false;
 		smatch matchingWord;
-		if((flag&MATCH_WHOLE_WORD)==0){
+		if ((flag&MATCH_WHOLE_WORD)==0) {
 			match = regex_search (lineData, matchingWord, expr);
 		}
-		else{
+		else {
 			string lineToMatch = lineData;
 			while (regex_search (lineToMatch, matchingWord, expr)) {
 		        string temp = matchingWord[0];
 		        int start = matchingWord.position();
-		        if( ( start==0 || !isalphaNum(lineToMatch[start-1]) ) &&  (start+temp.size()==lineToMatch.size() || !isalphaNum(lineToMatch[start+temp.size()]) ) ){
+		        if ((start == 0 || !isalphaNum(lineToMatch[start - 1]))
+			            && (start + temp.size() == lineToMatch.size() || !isalphaNum(lineToMatch[start + temp.size()]))) {
 		        	match = true;
 		        	break;
 				}
 		        lineToMatch = matchingWord.suffix();
 		    }
 		}
-		if(flag&INVERT_MATCH){
-			if(match==false){
-				if(flag&PRINT_LINE_NO){
+		if (flag & INVERT_MATCH) {
+			if (match == false) {
+				if (flag&PRINT_LINE_NO) {
 					cout<<lineNo<<": ";
 				}
 				cout<<lineData<<endl;
 			}
 		}
-		else{
-			if(match){
-				if(flag&PRINT_LINE_NO){
+		else {
+			if (match) {
+				if (flag&PRINT_LINE_NO) {
 					cout<<lineNo<<": ";
 				}
 				cout<<lineData<<endl;
@@ -79,52 +82,56 @@ void printMatch(int& flag, string patternToSearch, string fileName){
 	}
 }
 
-bool isValidOption(string option){
-	if(option=="-v" || option=="--invert-match" || option=="-i" || option=="--ignore-case" || option=="-w" || option=="--word-regexp" || option=="-n" || option=="--line-number")
-		return true;
+bool isValidOption(string option) {
+	if(option == "-v" || option == "--invert-match" || option == "-i" || option == "--ignore-case"
+	        || option == "-w" || option == "--word-regexp" || option == "-n" || option == "--line-number") {
+		return true;	
+	}
 	return false;
 }
 
-void updateFlag(int& flag, string option){
-	if(option=="-i" || option=="--ignore-case"){
-		flag|=IGNORE_CASE;
+void updateFlag(int& flag, string option) {
+	if (option == "-i" || option == "--ignore-case") {
+		flag |= IGNORE_CASE;
 	}
-	if(option=="-v" || option=="--invert-match"){
-		flag|=INVERT_MATCH;
+	if (option == "-v" || option == "--invert-match") {
+		flag |= INVERT_MATCH;
 	}
-	if(option=="-w" || option=="--word-regexp"){
-		flag|=MATCH_WHOLE_WORD;
+	if (option == "-w" || option == "--word-regexp") {
+		flag |= MATCH_WHOLE_WORD;
 	}
-	if(option=="-n" || option=="--line-number"){
-		flag|=PRINT_LINE_NO;
+	if (option == "-n" || option == "--line-number") {
+		flag |= PRINT_LINE_NO;
 	}
 }
 
-int main(int argc, char *argv[])
-{
-	while(1){
+int main(int argc, char *argv[]) {
+	while(1) {
 		cout<<endl;
-		cout<<"Type help for valid command. Type exit to terminate programme. Type [options] [patern] [file_name] to search pattern in <file_name> file."<<endl;
+		cout<< "Type help for valid command. Type exit to terminate programme. Type [options] [patern]"
+		        << "[file_name] to search pattern in <file_name> file."<<endl;
 		string input;
 		getline(cin, input);
-		if(input=="exit")
+		if (input == "exit") {
 			return 0;
-		if(input=="help"){
+		}
+		if (input == "help") {
 			prinHelp();
 			continue;
 		}
 		vector<string> args;
 		stringstream ss(input);
 		string temp;
-		while(ss>>temp)
+		while (ss>>temp) {
 			args.push_back(temp);
-		if(args.size()<2){
+		}
+		if (args.size() < 2) {
 			cout<<"Invalid Command."<<endl;
 			continue;
 		}
 		int flag=0;
 		bool valid=true;
-		for(int i=0;i<=(int)args.size()-3;i++){
+		for (int i=0;i<=(int)args.size()-3;i++) {
 			if(!isValidOption(args[i])){
 				cout<<args[i]<<" is not a valid option."<<endl;
 				valid=false;
@@ -132,8 +139,9 @@ int main(int argc, char *argv[])
 			}
 			updateFlag(flag, args[i]);
 		}
-		if(valid==false)
+		if (valid == false) {
 			continue;
+		}
 		printMatch(flag, args[args.size()-2], args[args.size()-1]);
 	}
 }
